6770d2e8810b230b80f0cda1667a84ad24598672
correctgavin@gmail.com <correctgavin@gmail.com>
2022-08-26 10:12:07 +0930

    machine count

--- RenaldoProductionManagementPlatform/RenaldoProductionManagementPlatform/ViewModel/MainViewModel.cs
+++ RenaldoProductionManagementPlatform/RenaldoProductionManagementPlatform/ViewModel/MainViewModel.cs
@@ -10,11 +10,24 @@ namespace RenaldoProductionManagementPlatform.ViewModel
     internal class MainViewModel : NotifyBase
     {
         private object _pageContent;
+        private Random _random = new Random();
 
         public object PageContent
         {
             get { return _pageContent; }
             set { SetProperty(ref _pageContent, value); }
+        }
+
+        private string _totalCount;
+        public string TotalCount
+        {
+            get { return _totalCount; }
+            set { SetProperty(ref _totalCount, value); }
+        }
+
+        public MainViewModel()
+        {
+            TotalCount = _random.Next(0, 5000).ToString("00000");
         }
     }
 }
